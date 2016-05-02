a2df7a0863091ca04539bc4bbaf97ab6912132a1
CodingFish <CodingFishWu.gmail.com>
2016-05-02 09:41:04 +0800

    add host of mongo, to support docker

--- src/main/java/daoImpl/ConnectDB.java
+++ src/main/java/daoImpl/ConnectDB.java
@@ -7,7 +7,7 @@ import com.mongodb.client.MongoDatabase;
  * Created by fish on 3/27/16.
  */
 public class ConnectDB {
-    public static MongoClient mongoClient = new MongoClient();
+    public static MongoClient mongoClient = new MongoClient("mongo", 27017);
     public static String name = "Bookstore";
 
     public static MongoDatabase getDB() {
